9d6d31bea989ba475a34629597654805c1fe31a7
MaynardFH <joaomaynard.tc@gmail.com>
2026-09-10 20:44:07 -0300

    Cria estrutura inicial do projeto

---
+++ app.py
@@ -0,0 +1,13 @@
+def main():
+    print("================================")
+    print("     GERENCIADOR DE TAREFAS")
+    print("================================")
+    print("1 - Listar tarefas")
+    print("2 - Adicionar tarefa")
+    print("3 - Concluir tarefa")
+    print("4 - Remover tarefa")
+    print("5 - Sair")
+
+
+if __name__ == "__main__":
+    main()
